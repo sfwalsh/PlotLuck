5294987253e2c5789b9b8b2fa561239b723fca19
Stephen Walsh <sfwalshappdev@gmail.com>
2024-02-12 15:44:59 +0000

    Initial Commit

---
+++ PlotLuck/PlotLuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlotLuckApp.swift
+//  PlotLuck
+//
+//  Created by Stephen Walsh on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlotLuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
