34531dff1e3695c57b64aefe2039bb00fdb88ff5
Stephen Walsh <sfwalshappdev@gmail.com>
2024-02-12 17:17:20 +0000

    Create ReadingListRepository.swift

---
+++ PlotLuck/Data/Repository/ReadingListRepository.swift
@@ -0,0 +1,16 @@
+//
+//  ReadingListRepository.swift
+//  PlotLuck
+//
+//  Created by Stephen Walsh on 12/02/2024.
+//
+
+import Foundation
+
+struct ReadingListRepository: CRUDRepository {
+    typealias Item = ReadingListItem
+    
+    func addItem(item: ReadingListItem) {
+        
+    }
+}
