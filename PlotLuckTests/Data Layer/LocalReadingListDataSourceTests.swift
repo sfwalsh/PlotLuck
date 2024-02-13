78d06e4d2a71733bcefaa1002070c6033f0db7e9
Stephen Walsh <sfwalshappdev@gmail.com>
2024-02-13 18:38:02 +0000

    Update LocalReadingListDataSourceTests to test fetch function

--- PlotLuckTests/Data Layer/LocalReadingListDataSourceTests.swift
+++ PlotLuckTests/Data Layer/LocalReadingListDataSourceTests.swift
@@ -14,7 +14,7 @@ final class LocalReadingListDataSourceTests: XCTestCase {
     
     private var modelContainer: ModelContainer?
     private var modelContext: ModelContext?
-    private var sut: LocalReadingListDatasource?
+    private var sut: LocalReadingListDatasource!
     
     override func setUpWithError() throws {
         let config = ModelConfiguration(isStoredInMemoryOnly: true)
@@ -24,28 +24,56 @@ final class LocalReadingListDataSourceTests: XCTestCase {
         sut = LocalReadingListDatasource(modelContext: modelContext!)
     }
 
-    func testAddItem_Success() async throws {
-        // Given
-        let itemsBefore = try fetchItemsFromContext()
-        XCTAssert(itemsBefore.isEmpty, "Items should be empty")
-        
-        // When I insert an item
+    func testAddItem_Success() throws {
+        // When I add an item
         let item = ReadingListItem(
             book: .init(title: "", author: "", isbn: ""),
             status: .unread
         )
-        try await sut?.addItem(item: item)
-        
-        let itemsAfter = try fetchItemsFromContext()
+        try sut.addItem(item: item)
+        let itemsInContext = try fetchItemsDirectlyFromContext()
         
         // Then
-        XCTAssert(itemsAfter.count == 1, "Items should have one item")
-        XCTAssertEqual(itemsAfter.first!, item, "The item should match that passed into the sut")
+        XCTAssert(itemsInContext.count == 1, "Items should have one item")
+        XCTAssertEqual(itemsInContext.first!, item, "The item should match that passed into the sut")
     }
     
-    // convienence function for retrieving current items in the SwiftData layer
-    private func fetchItemsFromContext() throws -> [ReadingListItem] {
+    func testFetchItems_EmptyState() throws {
+        // Given no items have been added
+        // When I call fetch
+        let items = try sut.fetchItems()
+        // No items are returned
+        XCTAssert(items.isEmpty, "items are empty")
+    }
+    
+    func testFetchItems_Success() throws {
+        // Given an item is in the context
+        let item = ReadingListItem(
+            book: .init(title: "", author: "", isbn: ""),
+            status: .unread
+        )
+        try insertItemDirectlyToContext(item: item)
+        
+        // When I call fetch
+        let results = try sut.fetchItems()
+        
+        // then that item is included in the returned results
+        XCTAssertEqual(results.first!, item, "The inserted item and the only returned item should be equal")
+    }
+}
+
+
+// MARK: Helper functions
+
+extension LocalReadingListDataSourceTests {
+    
+    private func fetchItemsDirectlyFromContext() throws -> [ReadingListItem] {
         let descriptor = FetchDescriptor<ReadingListItem>()
         return try modelContext!.fetch(descriptor)
+    }
+    
+    private func insertItemDirectlyToContext(item: ReadingListItem) throws {
+        modelContext?.insert(item)
+        try modelContext?.save()
     }
 }
