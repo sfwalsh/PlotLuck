2fc482b63973320e0c951db2ce816c72d4db5efa
Stephen Walsh <sfwalshappdev@gmail.com>
2024-02-15 17:27:54 +0000

    Tweak GoogleBookSearchResult to allow for optional values
    items without a title, author and isbn are filtered, rather than throwing a decoding error on the whole set of results

--- PlotLuck/Data/Models/API/GoogleBookSearchResult.swift
+++ PlotLuck/Data/Models/API/GoogleBookSearchResult.swift
@@ -12,15 +12,31 @@ struct GoogleBookSearchResponse: Decodable {
     
     // Note the model is incomplete and only stores data needed by the app.
     struct VolumeInfo: Decodable {
-        let title: String
-        let authors: [String]
-        let industryIdentifiers: [IndustryIdentifiers]
+        
+        enum CodingKeys: String, CodingKey {
+            case title, authors, industryIdentifiers
+        }
+        
+        let title: String?
+        let authors: [String]?
+        let industryIdentifiers: [IndustryIdentifiers]?
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            title = try container.decodeIfPresent(String.self, forKey: .title)
+            authors = try container.decodeIfPresent([String].self, forKey: .authors)
+            industryIdentifiers = try container.decodeIfPresent([IndustryIdentifiers].self, forKey: .industryIdentifiers)
+        }
     }
     
     struct IndustryIdentifiers: Decodable {
         let type, identifier: String
     }
     
-    let id: String
-    let volumeInfo: VolumeInfo
+    struct Item: Decodable {
+        let id: String
+        let volumeInfo: VolumeInfo
+    }
+    
+    let items: [Item]
 }
