48248d92236765787886f2c95ade15741d4667af
Stephen Walsh <sfwalshappdev@gmail.com>
2024-02-18 14:15:07 +0000

    Adds descriptions to Network Error

--- PlotLuck/Data/Network/NetworkError.swift
+++ PlotLuck/Data/Network/NetworkError.swift
@@ -10,4 +10,13 @@ import Foundation
 enum NetworkError: Error, LocalizedError {
     case badURL
     case invalidSearchTerm
+    
+    var errorDescription: String? {
+        switch self {
+        case .badURL:
+            "An error occurred while accessing the network"
+        case .invalidSearchTerm:
+            "An error occurred while accessing the network, please try another search term"
+        }
+    }
 }
